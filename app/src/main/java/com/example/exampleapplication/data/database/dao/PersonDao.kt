b3f72f3ac3d1ecb3cc70fbee572b1d66556fcde4
ysur67 <rus.muratow@gmail.com>
2021-09-04 14:20:55 +0300

    fix: daos with coroutines

--- app/src/main/java/com/example/exampleapplication/data/database/dao/PersonDao.kt
+++ app/src/main/java/com/example/exampleapplication/data/database/dao/PersonDao.kt
@@ -5,14 +5,15 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.exampleapplication.data.model.person.Person
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PersonDao {
     @Query("SELECT * FROM persons")
-    fun getAll() : List<Person>
+    fun getAll() : Flow<List<Person>>
 
     @Query("SELECT * FROM persons WHERE id IN (:personIds)")
-    fun loadAllByIds(personIds: IntArray)
+    fun loadAllByIds(personIds: IntArray) : Flow<List<Person>>
 
     @Insert
     fun insertAll(vararg persons: Person)
--- app/src/main/java/com/example/exampleapplication/data/database/dao/PostDao.kt
+++ app/src/main/java/com/example/exampleapplication/data/database/dao/PostDao.kt
@@ -5,11 +5,12 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.exampleapplication.data.model.Post
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PostDao {
     @Query("SELECT * FROM post")
-    fun getAll() : List<Post>
+    fun getAll() : Flow<List<Post>>
 
     @Insert
     fun insertAll(vararg post: Post)
